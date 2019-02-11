fe7cfcc0aa3309b63922c719ef25d311f01004b2
donaldRwilliams <drwwilliams@ucdavis.edu>
2019-02-11 15:50:07 -0800

    fisher z to ggmnonreg

--- R/or_helper.R
+++ R/or_helper.R
@@ -25,3 +25,47 @@ quantile_helper <- function(x, y){
   ifelse(x < 0 &  y > 0, 0, 1)
 }
 
+ci_par_cor <- function(alpha, par_cors, s, n) {
+
+  # n: sample size
+  # s: p - 1 (controlled for)
+  # alpha: confidence level
+  # par_cors: partial correlations
+  mat <- pmat <- matrix(0,nrow =  s + 1, ncol = s + 1)
+  CI_ls <- list()
+  par_cor <- par_cors[upper.tri(par_cors)]
+  cov <- list()
+  pvalues <-list()
+  for(i in 1:length(par_cor)){
+    # crtiical value
+    z_crit <- qnorm(1 - alpha/2)
+    # standard error
+    se <- sqrt(1/((n - s - 3)))
+    # z transformation
+    z <- log((1 + par_cor[i])/(1 - par_cor[i]))/2
+    # z lower bound
+    Z_L <- z - z_crit * se
+    # Z upper bound
+
+    Z_U <- z + z_crit * se
+    rho_L <- (exp(2*Z_L) - 1)/(exp(2*Z_L) + 1)
+    rho_U <- (exp(2*Z_U) - 1)/(exp(2*Z_U) + 1)
+    CI <- c(rho_L, rho_U)
+    CI_ls[[i]] <- CI
+    cov[[i]] <- ifelse(CI[1] < 0 & CI[2] > 0, 0, 1)
+    pvalues[[i]] <- 2 * pnorm(abs(z / se), lower.tail = F)
+  }
+
+  c_dat <- do.call(rbind.data.frame, CI_ls)
+  colnames(c_dat) <- c("low", "up")
+  c_dat$pcar <- unlist(par_cor)
+  diag(mat) <- 1
+  mat[upper.tri(mat)] <- unlist(cov)
+  pmat[upper.tri(pmat)] <- unlist(pvalues)
+
+  mat <- as.matrix(Matrix::forceSymmetric(mat))
+  pmat <- as.matrix(Matrix::forceSymmetric(pmat))
+
+  list(sig_mat = mat, par_cors = par_cors, par_sig = mat * par_cors,
+       cis = c_dat, cov_prob = unlist(cov), pmat = pmat)
+}
---
+++ R/parcor_fisher_z.R
@@ -0,0 +1,48 @@
+#' Title
+#'
+#' @param X
+#' @param alpha
+#' @param FDR
+#'
+#' @return
+#' @export
+#'
+#' @examples
+parcor_fisher_z <- function(X, alpha, FDR = NULL){
+
+
+  n <- nrow(X)
+  p <- ncol(X)
+  I = diag(1,p)
+  ## compute maximum likelihood estimator for covariance matrix
+  mle_cov <- crossprod(scale(X, scale = F)) / n
+
+  ## compute maximum likelihood estimator of precision matrix
+  ## (inverse covariance matrix)
+  mle_inv <- solve(mle_cov)
+
+  ## standardize and revese sign = partial correaltions
+  pc  <-  as.matrix(cov2cor(mle_inv))  * - 1
+
+  mle_parcors <- ci_par_cor(alpha = alpha, par_cors = pc, n = n, s = p - 1)
+  mle_inv <- mle_parcors$sig_mat * mle_inv
+
+
+
+  if(isTRUE(FDR)){
+    mat_fdr <- matrix(0, p, p)
+    pvalues <- mle_parcors$pmat[upper.tri(mle_parcors$pmat)]
+    mat_fdr[upper.tri(mat_fdr)] <- ifelse(stats::p.adjust(pvalues, method = "fdr") < alpha, 1, 0)
+    mat_fdr <- as.matrix(Matrix::forceSymmetric(mat_fdr))
+
+    list(mle_parcors = mle_parcors, mle_inv = mle_inv , mat_fdr = mat_fdr)
+  } else{
+
+    list(mle_parcors = mle_parcors, mle_inv = mle_inv)
+
+  }
+
+
+
+
+}
