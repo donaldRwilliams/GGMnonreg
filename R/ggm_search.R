ec6ad2203532ef55bef22e7c2b3a49a75bd3a29f
Donald R. Williams <brycewayne03.29@gmail.com>
2021-03-31 06:12:59 -0700

    add approx_L0

--- R/ggm_search.R
+++ R/ggm_search.R
@@ -1,17 +1,43 @@
 #' Gaussian graphical model: automated search
 #'
-#' @param Y  The data matrix of dimensions \emph{n} (observations) by \emph{p} (nodes).
+#' @param x  A data matrix of dimensions \emph{n} (observations) by \emph{p} (nodes) or
+#'           a correlation matrix of dimensions \emph{p} by \emph{p}.
 #'
 #' @param IC Character string. The desired information criterion. Options include
 #'           \code{"AIC"} and \code{"BIC"} (default).
 #'
-#' @param method Character string. The desired subsect selection method
+#' @param type Character string. Which search method should be used? The options included
+#'             \code{"regression"} and \code{"approx_L0"}. See details.
+#'
+#'
+#' @param method Character string. The desired subset selection method
 #'               Options includes \code{"forward"} (default), \code{"backward"},
 #'               and \code{"exhaustive"}.
 #'
+#' @param n Integer. Sample size. Required if a correlation matrix is provided.
+#'
+#' @references
+#' \insertAllCited{}
+#'
 #' @return An object of class \code{ggm_search}.
 #'
-#' @note Multiple regression is used to estimate the graph.
+#'
+#' @note \code{type = "neighborhood_selection"} employs multiple regression to estimate
+#' the graph (requires the data), whereas \code{type = "approx_L0"} directly estimates
+#' the precision matrix (data or a correlation matrix are acceptable). If
+#' data is provided and \code{type = "approx_L0"}, by default Pearson correlations are
+#' used. For another correlation coefficient, provide the desired correlation matrix.
+#'
+#' \code{type = "approx_L0"} is a continuous approximation to (non-regularized)
+#' best subset model selection. This is accomplished by using regularization, but
+#' the penalty (approximately) mimics non-regularized estimation.
+#'
+#'
+#' @details \code{type = "neighborhood_selection"} was described in
+#' \insertCite{williams2019nonregularized;textual}{GGMnonreg}
+#' and \code{type = "approx_L0"} was described in \insertCite{williams2020beyond;textual}{GGMnonreg}.
+#' The penalty for \code{type = "approx_L0"} is called seamless L0 \insertCite{dicker2013variable}{GGMnonreg}
+#'
 #'
 #' @export
 #'
@@ -25,58 +51,103 @@
 #' }
 #'
 #' @importFrom bestglm bestglm
-ggm_search <- function(Y, IC = "BIC", method = "forward") {
+#' @importFrom GGMncv ggmncv
+ggm_search <- function(x, IC = "BIC",
+                       type = "neighborhood_selection",
+                       method = "forward",
+                       n = NULL) {
 
-  if(!IC %in% c("AIC", "BIC")){
+  if(!IC %in% c("AIC", "BIC")) {
     stop("IC must be 'AIC' or 'BIC'")
   }
 
-  if(!method %in% c("forward", "backward", "exhaustive")){
-    stop("method must be 'forward', 'backward', or 'exhaustive'")
-  }
+  if(type == "neighborhood_selection") {
+    if (isSymmetric(as.matrix(x))) {
+      stop("data required for 'neighborhood_selection'")
+    }
 
-  # change to X
-  X <- Y
-  X <- scale(X, scale = T)
-  n <- nrow(X)
-  p <- ncol(X)
+    if (!method %in% c("forward", "backward", "exhaustive")) {
+      stop("method must be 'forward', 'backward', or 'exhaustive'")
+    }
 
-  betas <- matrix(0, p, p)
+    # change to X
+    X <- x
+    X <- scale(X, scale = T)
+    n <- nrow(X)
+    p <- ncol(X)
 
-  colnames(betas) <- paste("X", 1:p, sep = "")
+    betas <- matrix(0, p, p)
 
-  colnames(X) <-  paste("X", 1:p, sep = "")
+    colnames(betas) <- paste("X", 1:p, sep = "")
 
-  pb <- txtProgressBar(min = 0, max = p, style = 3)
+    colnames(X) <-  paste("X", 1:p, sep = "")
 
-  estimates <- lapply(1:p, function(x) {
+    pb <- txtProgressBar(min = 0, max = p, style = 3)
 
-    est_i <- bestglm::bestglm(
-      cbind.data.frame(X[, -x], X[, x]),
-      method = method,
-      IC = IC,
-      intercept = F
-    )$BestModel$coefficients
+    estimates <- lapply(1:p, function(x) {
+      est_i <- bestglm::bestglm(
+        cbind.data.frame(X[,-x], X[, x]),
+        method = method,
+        IC = IC,
+        intercept = F
+      )$BestModel$coefficients
 
-    setTxtProgressBar(pb, x)
+      setTxtProgressBar(pb, x)
 
-    est_i
+      est_i
     })
 
-  for(i in 1:p){
-    betas[i, names(estimates[[i]])] <- estimates[[i]]
+    for (i in 1:p) {
+      betas[i, names(estimates[[i]])] <- estimates[[i]]
 
     }
 
-  wadj <- sqrt(betas * t(betas)) * sign(betas)
-  adj <- ifelse(wadj == 0, 0, 1)
+    wadj <- sqrt(betas * t(betas)) * sign(betas)
+    adj <- ifelse(wadj == 0, 0, 1)
+
+
+  } else if (type == "approx_L0") {
+
+    p <- ncol(x)
+
+    if (!isSymmetric(as.matrix(x))) {
+      n <- nrow(x)
+      R <- cor(x)
+    } else {
+      if (is.null(n)) {
+        stop("sample size required when a correlation matrix is provided")
+      }
+
+      R <- x
+    }
+
+    if (IC == "AIC") {
+      IC <- "aic"
+    } else {
+      IC <- "bic"
+    }
+
+    # seamless L0 penalty
+    fit <-
+      GGMncv::ggmncv(
+        R,
+        n = n,
+        ic = IC,
+        penalty = "selo",
+        unreg = TRUE
+      )
+
+    wadj <- fit$P
+    adj <- fit$adj
+
+    }
 
   returned_object <- list(
     wadj = wadj,
     adj = adj,
     p = p,
     n = n,
-    Y = Y
+    x = x
   )
 
   class(returned_object) <- c("ggmnonreg",
